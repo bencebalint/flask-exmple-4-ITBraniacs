6f6f647c79bd09be2ea2a02057fcf2d546125d44
Bence Balint <bence.balint@telenav.com>
2018-02-06 18:04:46 +0200

    Added support for web requests

--- school/routes/web_route.py
+++ school/routes/web_route.py
@@ -1,4 +1,4 @@
-from flask import Response, render_template, url_for
+from flask import Response, send_file
 
 from routes.main_route import MainRoute
 
@@ -12,8 +12,14 @@ class WebRoute(MainRoute):
     def _generate_response(cls, input_data=None) -> Response:
         return super()._generate_response(input_data)
 
-    def get(self, element) -> Response:
-        return render_template(element)
+    def get(self, element: str) -> Response:
+        location = ""
+        if element.__contains__("js"):
+            location = "templates/script/"
+        elif element.__contains__("css"):
+            location = "templates/css/"
+
+        return send_file(location + element)
 
     def post(self) -> Response:
         return super().post()
